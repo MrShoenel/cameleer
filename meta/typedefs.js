c8cdbec8cfef3804b461e5550c6deea567eb87da
Sebastian Hönel <sehoaa@lnu.se>
2018-07-10 16:47:15 +0200

    Replace the concept of LastFunctionArg with Result and let functional tasks access their CameleerJob

--- meta/typedefs.js
+++ meta/typedefs.js
@@ -2,8 +2,7 @@
 // , shot = require('sh.orchestration-tools')
 // , IntervalClass = shot.Interval
 // , ProgressClass = shot.Progress
-// , ScheduleClass = shot.Schedule
-// , LastFunctionArgClass = require('./LastFunctionArg').LastFunctionArg;
+// , ScheduleClass = shot.Schedule;
 
 
 /**
@@ -25,7 +24,7 @@
  * @typedef ErrorArg
  * @type {Function}
  * 
- * @typedef LastFunctionArg
+ * @typedef CameleerJob
  * @type {Function}
  */
 
@@ -43,6 +42,7 @@
  * @typedef FunctionalTaskErrorConfig
  * @type {Object}
  * @property {() => (Schedule|Promise.<Schedule>)} schedule A schedule that is used to retry this task after it failed. The task will be retried for as long as the schedule given schedules it or until it succeeds.
+ * @property {number|(() => (number|Promise.<number>)} [maxNumFails] Optional. Defaults to Number.MAX_SAFE_INTEGER. If a task fails, its fail-counter is increased. If maxNumFails is specified and the amount of fails reaches that value, that task is considered having failed finally.
  * @property {boolean|(() => (boolean|Promise.<boolean>)} [skip] Optional. Defaults to 'false'. This property is evaluated every time when the task is scheduled to run according to the given schedule. If you specify a literal boolean value, a failed task can be skipped after it failed the first time, i.e. if you provide 'true', the task will be skipped after initial failure immediately without having to wait for its rescheduling.
  * @property {boolean|(() => (boolean|Promise.<boolean>)} [continueOnFinalFail] Optional. Defaults to 'false'. This property determines how to continue after this task finally failed (i.e. no attempts are left). Providing 'true' will lead to the program continuing with the next task. 'false' on the other hand will stop the entire task and prevent any more sub-tasks from executing.
  */
@@ -53,9 +53,9 @@
  * @typedef FunctionalTaskConfig
  * @type {Object}
  * @property {boolean|FunctionalTaskErrorConfig} [canFail] Optional. Defaults to Cameleer's configuration for FunctionalTaskErrorConfig. Whether or not this task may fail. You may either specify a boolean value or give a more detailed definition using a FunctionalTaskErrorConfig for the case when this task fails.
- * @property {(...args: Array.<Value|LastFunctionArg>) => (Value|Promise.<Value>)} func The (async) function to execute within this functional task.
+ * @property {(...args: Array.<Value|CameleerJob>) => (Value|Promise.<Value>)} func The (async) function to execute within this functional task.
  * @property {Object} [thisArg] Optional. Defaults to 'null'. The this-argument to bind the function to (not applicable to arrow-functions).
- * @property {Array.<Value>|(() => (Array.<Value>|Promise.<Array.<Value>>))} [args] Optional. Defaults to an empty array ('[]'). Arguments passed to the functional task, obtained literally, from a Function, or a Promise-producing function. Note that the last argument is always the result of the preceding task. If there was no preceding task, the last argument defaults to 'undefined'. The last argument is passed as an instance of LastFunctionArg.
+ * @property {Array.<Value>|(() => (Array.<Value>|Promise.<Array.<Value>>))} [args] Optional. Defaults to an empty array ('[]'). Arguments passed to the functional task, obtained literally, from a Function, or a Promise-producing function. Note that the last argument is always the result of the preceding task. If there was no preceding task, the last argument defaults to 'undefined'. The last argument is passed as an instance of CameleerJob. The CameleerJob provides access to all previous results, the task's logger and a shared object (a context).
  */
 
 
@@ -81,7 +81,7 @@
  * @property {Array.<string>|(() => (Array.<string>|Promise.<Array.<string>>)} [queues] Optional. Defaults to '[]'. An array of names of queues, this task is allowed to run on. Queues are checked in the order they appear and the first matching queue that has a free slot (in case of parallel queues) or enough capabilities is selected to run the job. If no such queue is found, the job is enqueued in the least busy queue. If this property does not return an array of strings or e.g. the promise is rejected, then the job is not run and aborted. This property is evaluated every time the task is scheduled to run.
  * @property {Progress|(() => (Progress|Promise.<Progress>))} [progress] Optional. Defaults to 'null'. A Progress-object that will be observed for progress, while this task is running. This property is evaluated every time the task is scheduled to run.
  * @property {Schedule|(() => (Schedule|Promise.<Schedule>))} schedule The schedule this job uses to schedule when it should be triggered. This schedule will internally be added to an appropriate scheduler. Note that this property is only evaluated once during task creation (i.e. the schedule of a task cannot be changed later).
- * @property {SimpleTaskConfig|(() => (SimpleTaskConfig|Promise.<SimpleTaskConfig>))} tasks An array of functions, promise-producing functions or functional-tasks to run as the main task of this definition. The tasks are run in the order they appear in the array, one after another. Execution is therefore serial, not parallel or asynchronous (however, each task may be an async function/Promise-producing function). The value returned by one task is passed as last argument to the next task (i.e. there is always one argument passed). The first task is passed 'undefined' as last argument, as there was no previous task. The final value is discarded. This property is evaluated every time the task is scheduled to run.
+ * @property {SimpleTaskConfig|(() => (SimpleTaskConfig|Promise.<SimpleTaskConfig>))} tasks An array of functions, promise-producing functions or functional-tasks to run as the main task of this definition. The tasks are run in the order they appear in the array, one after another. Execution is therefore serial, not parallel or asynchronous (however, each task may be an async function/Promise-producing function). The value returned by one task is added to the CameleerJob's results. The CameleerJob is passed as last argument to the next task (i.e. there is always one argument passed). If there were no previous results yet, the CameleerJob's intermediate results will be empty (and its result-property will return undefined). The final value is then also represented by CameleerJob::result.
  */
 
 
